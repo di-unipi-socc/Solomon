b7dc236025f059a5e8d0e5d1fe2e4fa522b1e8ba
GBisi <giuseppe.bisicchia@outlook.it>
2021-01-15 21:32:35 +0100

    Added Smart Home Example

--- Examples/Smart Home/infr.pl
+++ Examples/Smart Home/infr.pl
@@ -1,17 +1,20 @@
 % Sample data: static %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%propertyType(TypeId).
+% propertyType(TypeId).
 propertyType(light).
 
-%actuator(AId, TypeId).
+% actuator(AId, TypeId).
 actuator(wardrobeLight1, light).
 actuator(bedLight1, light).
 actuator(mainLight1,   light).
 actuator(cornerLight1,   light).
 
-%sensor(SId, TypeId).
+% sensor(SId, TypeId).
 sensor(brightness1,  light).
 
-% propertyInstance(ZoneId, PropertyInstanceId, ListOfActuators, ListOfSensors)
+% zone(ZId, AppliedPolicy).
+zone(bedroom, _).
+
+% propertyInstance(ZoneId, PropertyInstanceId, PropertyType, ListOfActuators, ListOfSensors)
 propertyInstance(bedroom, morningLight,   light,  [wardrobeLight1],                          [brightness1]).
 propertyInstance(bedroom, nightLight,     light,  [bedLight1],                               [brightness1]).
 propertyInstance(bedroom, movieLight,     light,  [cornerLight1, bedLight1],                 [brightness1]).
@@ -19,9 +22,16 @@ propertyInstance(bedroom, workLight,      light,  [cornerLight1, mainLight1],   
 propertyInstance(bedroom, cleaningLight,  light,  [mainLight1],                              [brightness1]).
 propertyInstance(bedroom, dinnerLight,    light,  [cornerLight1, bedLight1, wardrobeLight1], [brightness1]).
 
+% user(UId, AllowedZonesLs).
+user(u1, [bedroom]).
+user(u2, [bedroom]).
 
 % Sample data: dynamic %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% sensorValue(SId, Value).
 sensorValue(brightness, 20). % out of 100
+
+% set(UId, ZId, PIId, Value).
 set(u1, bedroom, movieLight, 50).
 set(u1, bedroom, nightLight, 100).
 set(u2, bedroom, movieLight, 10).
---
+++ Examples/Smart Home/policies.pl
@@ -0,0 +1,51 @@
+:- consult('infr').
+:- consult('../../reasoner.pl').
+
+% mediateRequests/2  for each pair (Zone, PropertyInstance) obtains users' requests, 
+% averages them and then applies policies based on the propertyInstance and various environmental parameters.
+
+mediateRequests(Requests, Mediated) :-
+    groupPerPI(Requests, NewRequests),
+    mediateRequest(NewRequests, Mediated).
+
+mediateRequest([],[]).
+mediateRequest([(Z,PI,Ls)|Reqs], [Mediated|OtherMediatedReqs]) :-
+    mediatePI(Z,PI,Ls,Mediated),
+    mediateRequest(Reqs, OtherMediatedReqs).
+
+mediatePI(Z, PI, [], (Z, PI, undefined)).
+mediatePI(Z, PI, Ls, (Z, PI, Avg)) :-
+    findall(V, member((V,_),Ls), Values), % get values
+    avg(Values,Avg).
+
+
+% associateActions/2 given a set of (mediated) requests, returns a list of actions of the type (ActuatorId, Value). 
+% To do this, for each mediated request, 
+% assigns the action to be performed to each actuator of the propertyInstance. 
+% Once all requests have been "rolled out" it checks if an actuator has more than one action assigned 
+% and if so, mediate between those actions.
+associateActions(Requests, ExecutableActions) :-
+    actionsFor(Requests, Actions),             % returns a list of actions (ActuatorId, Value)
+    setActuators(Actions, ExecutableActions).  % [Defined by Admin] determines a single action for each actuator
+
+% actionsFor/2 for each mediated request takes the set of actuators and sensors assigned to the propertyInstance and 
+% for each of them assigns the corresponding action.
+actionsFor([],[]).
+actionsFor([(_, _, undefined)|Reqs], Actions) :-
+    actionsFor(Reqs,Actions).
+
+actionsFor([(Z, PI, V)|Reqs], Actions) :-
+    propertyInstance(Z, PI, _, ActuatorList, SensorList),              % given a Zone and its PropertyInstace takes the set of actuators and sensors
+    selectActionsForPI(Z, PI, V, ActuatorList, SensorList, Actions1),  % [Defined by Admin] for each actuator in the PropertyInstance assigns an action (ActuatorId, Value)    
+    actionsFor(Reqs, Actions2), append(Actions1, Actions2, Actions).   % combines these actions with the others
+
+% determineActions defined by SysAdmin (e.g. can use sensor values taken from the 4th param) %%%%%%%%%%%%%%%%%%%%%%%%%
+selectActionsForPI(_, _, V, ActuatorList, _, Actions) :-
+    triggerOneActuator(V, ActuatorList, Actions).
+
+triggerOneActuator(_, [], []).
+triggerOneActuator(V, [Actuator|_], [(Actuator,V)]).
+
+% resolveActions defined by SysAdmin, cannot read data from sensors (?) %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% (will mainly do avg, max, min, mode, cap) %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+setActuators(Actions, ExecutableActions) :- setActuatorsWithMode(Actions, 0, 100, ExecutableActions).
